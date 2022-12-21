c5cc0ecd4fcb985bcae8beeba4fb62eeb4410859
ramraina3 <119046476+ramraina3@users.noreply.github.com>
2022-12-21 17:37:19 +0530

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    snackbar:{
+      show: false
+    }
   },
   getters: {
   },
   mutations: {
+    showSnackbar(state){
+      state.snackbar.show = true
+    }
   },
   actions: {
   },
